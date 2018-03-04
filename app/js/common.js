f0786c83efe325d839640565282d2b81808434a0
Wra17h <extr1m3@mail.ru>
2018-03-04 23:24:56 +0400

    Fix owlNav, fix fonts in process

--- app/js/common.js
+++ app/js/common.js
@@ -1,28 +1,47 @@
 $(document).ready(function () {
 
-    $(".slider__up").owlCarousel({
-        loop: true,
+    var owlUp = $(".slider__up"); //блок с верхней каруселью
+    var owlDown = $(".slider__down"); //блок с нижней каруселью
+
+    //Инициализируем карусель в верхнем слайдере
+    owlUp.owlCarousel({
+        // loop: true,
         center: true,
         items: 1,
         videoWidth: 1920,
         videoHeight: 668,
-        // video: true,
         autoplay: false,
-        nav: true,
         navText: ["<img class=\"arrow__left\" src=\"img/arrows.svg\">","<img class=\"arrow__right\" src=\"img/arrows.svg\">"]
     });
 
-    $(".slider__down").owlCarousel({
-        loop: true,
+    //Добавляем к кнопкам в блоке с верхней каруселью обработчики
+    $(".up__right").click(function() {
+        owlUp.trigger('next.owl.carousel');
+    });
+    $(".up__left").click(function() {
+        owlUp.trigger('prev.owl.carousel', [300]);
+    });
+
+    //Инициализируем карусель в нижнем слайдере
+    owlDown.owlCarousel({
+        // loop: true,
         center: true,
         video: true,
         items: 1,
         videoWidth: 1920,
         videoHeight: 500,
         autoplay: false,
-        nav: true,
         navText: ["<img class=\"arrow__left\" src=\"img/arrows.svg\">","<img class=\"arrow__right\" src=\"img/arrows.svg\">"]
     });
+
+    //добавляем к кнопкам в блоке с нижней каруселью обработчики
+    $(".down__right").click(function() {
+        owlDown.trigger('next.owl.carousel');
+    });
+    $(".down__left").click(function() {
+        owlDown.trigger('prev.owl.carousel', [300]);
+    });
+
 
     $(".mfp-gallery").magnificPopup({
         type: 'image',
